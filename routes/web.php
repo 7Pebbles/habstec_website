74bb1b74e551ddc9240a3382d579fe23c27aec06
QaziTalha4595 <97889460+QaziTalha4595@users.noreply.github.com>
2023-10-04 12:00:30 +0530

    yes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@
 |
 */
 
-$router->get('/', function () {
-    return view('index');
+$router->get('/', function () use ($router) {
+    return $router->app->version();
 });
+
+
+$router->get('Admin/Login',["as"=>"Login", "uses"=>"AjaxController@Login"]);
+$router->post('Admin/LoginUser',["as"=>"LoginUser", "uses"=>"AjaxController@LoginUser"]);
+$router->group(['prefix' => 'Admin', 'middleware'=> 'auth'], function () use ($router) {
+    $router->get('/Dashboard',["as"=>"Dashboard", "uses"=>"AjaxController@Dashboard"]);
+
+    $router->get('/Blog',["as"=>"Blog", "uses"=>"AjaxController@Blog"]);
+    $router->post('/BlogInsert',["as"=>"BlogInsert", "uses"=>"AjaxController@BlogInsert"]);
+    $router->get('/BlogFetch',["as"=>"BlogFetch", "uses"=>"AjaxController@BlogFetch"]);
+    $router->get('/BlogDelete',["as"=>"BlogDelete", "uses"=>"AjaxController@BlogDelete"]);
+
+    $router->get('/Gallery',["as"=>"Gallery", "uses"=>"AjaxController@Gallery"]);
+    $router->post('/GalleryInsert',["as"=>"GalleryInsert", "uses"=>"AjaxController@GalleryInsert"]);
+    $router->get('/GalleryFetch',["as"=>"GalleryFetch", "uses"=>"AjaxController@GalleryFetch"]);
+    $router->get('/GalleryDelete',["as"=>"GalleryDelete", "uses"=>"AjaxController@GalleryDelete"]);
+
+    $router->get('/Contacts',["as"=>"Contacts", "uses"=>"AjaxController@Contacts"]);
+    $router->get('/ContactsFetch',["as"=>"ContactsFetch", "uses"=>"AjaxController@ContactsFetch"]);
+
+});
+
+$router->get('*', function () {
+abort(404);
+});
+
+
+
